23032f98f62eef5be0c34df2764130bc680b193f
Erik Pragt <erik.pragt@gmail.com>
2026-01-05 18:46:33 +1100

    Updated hash

--- Formula/allscreenshots.rb
+++ Formula/allscreenshots.rb
@@ -2,7 +2,7 @@ class Allscreenshots < Formula
   desc "Capture website screenshots from the command line"
   homepage "https://allscreenshots.com"
   url "https://github.com/allscreenshots/allscreenshots-cli/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "REPLACE_WITH_ACTUAL_SHA256_AFTER_RELEASE"
+  sha256 "8c6186a184472b3b83c42def32a7a805ac2d68eed64a3d82422c6a3753fb4ea0"
   license "Apache-2.0"
   head "https://github.com/allscreenshots/allscreenshots-cli.git", branch: "main"
 
